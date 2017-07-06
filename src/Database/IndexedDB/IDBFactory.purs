2ab8adb2190a772e52a8ba03d10db3aab291a4e6
KtorZ <matthias.benkort@gmail.com>
2017-07-06 14:12:45 +0200

    add documentation to IDBFactory

--- src/Database/IndexedDB/IDBFactory.purs
+++ src/Database/IndexedDB/IDBFactory.purs
@@ -1,12 +1,15 @@
+-- | Database objects are accessed through methods on the IDBFactory interface.
+-- | A single object implementing this interface is present in the global scope
+-- | of environments that support Indexed DB operations.
 module Database.IndexedDB.IDBFactory where
 
-import Prelude                     (Unit)
+import Prelude                 (Unit)
 
-import Control.Monad.Aff           (Aff)
-import Control.Monad.Eff           (Eff)
-import Data.Function.Uncurried      as Fn
-import Data.Function.Uncurried     (Fn4)
-import Data.Maybe                  (Maybe, fromMaybe)
+import Control.Monad.Aff       (Aff)
+import Control.Monad.Eff       (Eff)
+import Data.Function.Uncurried  as Fn
+import Data.Function.Uncurried (Fn4)
+import Data.Maybe              (Maybe, fromMaybe)
 
 import Database.IndexedDB.Core
 
@@ -14,18 +17,46 @@ import Database.IndexedDB.Core
 --------------------
 -- INTERFACE
 --
+-- Type alias for binding listeners to an initial open action
 type OpenRequest e =
   { onBlocked       :: Maybe (Eff (| e) Unit)
   , onUpgradeNeeded :: Maybe (Database -> Transaction -> Eff (| e) Unit)
   }
 
 
-deleteDatabase :: forall e. String -> Aff (idb :: IDB | e) Int
+-- | Type alias for DatabaseName
+type DatabaseName = String
+
+
+-- | Type alias for Version
+type Version = Int
+
+
+-- | Attempts to delete the named database. If the database already exists
+-- | and there are open connections that don’t close in response to a
+-- | `versionchange` event, the request will be blocked until all they close.
+deleteDatabase
+    :: forall e
+    .  DatabaseName
+    -> Aff (idb :: IDB | e) Int
 deleteDatabase =
   _deleteDatabase
 
 
-open :: forall e e'.  String -> Maybe Int -> OpenRequest e' -> Aff (idb :: IDB | e) Database
+-- | Attempts to open a connection to the named database with the specified version.
+-- | If the database already exists with a lower version and there are open connections
+-- | that don’t close in response to a versionchange event, the request will be blocked
+-- | until all they close, then an upgrade will occur. If the database already exists with
+-- | a higher version the request will fail.
+-- |
+-- | When the version isn't provided (`Nothing`), attempts to open a connection to the
+-- | named database with the current version, or 1 if it does not already exist.
+open
+    :: forall e e'
+    .  DatabaseName
+    -> Maybe Version
+    -> OpenRequest e'
+    -> Aff (idb :: IDB | e) Database
 open name mver req =
   Fn.runFn4 _open fromMaybe name mver req
 
@@ -33,7 +64,12 @@ open name mver req =
 --------------------
 -- FFI
 --
-foreign import _deleteDatabase :: forall e. String -> Aff (idb :: IDB | e) Int
+foreign import _deleteDatabase
+    :: forall e
+    .  String
+    -> Aff (idb :: IDB | e) Int
 
 
-foreign import _open :: forall a e e'. Fn4 (a -> Maybe a -> a) String (Maybe Int) (OpenRequest e') (Aff (idb :: IDB | e) Database)
+foreign import _open
+    :: forall a e e'
+    .  Fn4 (a -> Maybe a -> a) String (Maybe Int) (OpenRequest e') (Aff (idb :: IDB | e) Database)
